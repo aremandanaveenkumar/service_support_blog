0e10ba9fb547876e6d3695b9753efdbb3dd52953
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-07-26 22:20:30 +0200

    Delete all migrations, all models for trying again

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import AddressField, Post, Comment
+# from .models import AddressField, Post, Comment
 
 # Register your models here.
-admin.site.register(AddressField)
-admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(AddressField)
+# admin.site.register(Post)
+# admin.site.register(Comment)
