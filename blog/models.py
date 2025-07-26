781ba292007d5017d53368655d4f22d97dfd1627
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-07-26 23:11:00 +0200

    Readd all code for models in models.py and admin.py

--- blog/models.py
+++ blog/models.py
@@ -1,48 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# STATUS = ((0, "Draft"), (1, "Published"), 
-#           (2, "NotRectified"), (3,  "Rectified"))
+STATUS = ((0, "Draft"), (1, "Published"), 
+          (2, "NotRectified"), (3,  "Rectified"))
 
-# # Create your models here.
-# class AddressField(models.Model):
-#     street = models.CharField(max_length=255, blank=True, null=True)
-#     city = models.CharField(max_length=100, blank=True, null=True)
-#     state = models.CharField(max_length=100, blank=True, null=True)
-#     postal_code = models.CharField(max_length=20, blank=True, null=True)
-#     country = models.CharField(max_length=100, blank=True, null=True)
+# Create your models here.
+class AddressField(models.Model):
+    street = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
 
-#     def __str__(self):
-#         return f"{self.street}, {self.city}, {self.state}, {self.country}"
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state}, {self.country}"
 
-# class Post(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
-#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-#     customer = models.CharField(max_length=100)
-#     customer_address = models.ForeignKey(AddressField, on_delete=models.CASCADE, related_name='+', blank=True, null=True)
-#     problem_reported = models.TextField()
-#     retification = models.TextField()
-#     created_on = models.DateTimeField(auto_now_add=True)
-#     status = models.IntegerField(choices=STATUS, default=0)
-#     excerpt = models.TextField(blank=True)
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    customer = models.CharField(max_length=100)
+    customer_address = models.ForeignKey(AddressField, on_delete=models.CASCADE, related_name='+', blank=True, null=True)
+    problem_reported = models.TextField()
+    retification = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
 
-#     class Meta:
-#         ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
                     
-#     def __str__(self):
-#         return f"{self.title} | written by {self.author}"
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
-# class Comment(models.Model):
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-#     body = models.TextField(default="Thanks!")
-#     approved = models.TextField(default=False)
-#     created_on = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField(default="Thanks!")
+    approved = models.TextField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
-#     class Meta:
-#         ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
                     
-#     def __str__(self):
-#         return f"Comment {self.body} by {self.author}"
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
     
