5987f0778529e90e7d31471b76fd64ec84d8a214
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-07-26 21:05:30 +0200

    Add meta class and __str__ return title and author to display post

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AddressField, Post
+from .models import AddressField, Post, Comment
 
 # Register your models here.
 admin.site.register(AddressField)
 admin.site.register(Post)
+admin.site.register(Comment)
