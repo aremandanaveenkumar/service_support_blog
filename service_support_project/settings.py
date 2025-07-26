73b0f81b28da7832bfa2ee86d06c7b426630c322
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-07-26 11:35:34 +0200

    Update Secret key in env and update in settings.py to retrive

--- service_support_project/settings.py
+++ service_support_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--bqh8n*%t5_f_s9j%bs@r&2ynyy6-8l=5k3$e+p3%05h=m(a5p'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
