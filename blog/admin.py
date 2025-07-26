2219be7061d579b48cb56389a9f1e658c6d8ef56
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-07-26 22:59:07 +0200

    Create new postgre database and add in env.py and create a new superuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import AddressField, Post, Comment
+# from .models import AddressField, Post, Comment
 
 # Register your models here.
-admin.site.register(AddressField)
-admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(AddressField)
+# admin.site.register(Post)
+# admin.site.register(Comment)
 
