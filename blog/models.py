0e602df10616315f0b857972995d6911bf576a31
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-07-26 12:59:08 +0200

    Create custom model for addressfield

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from address.models import AddressField
 
 STATUS = ((0, "Draft"), (1, "Published"), 
           (2, "NotRectified"), (3,  "Rectified"))
 
 # Create your models here.
+class AddressField(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state}, {self.country}"
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
