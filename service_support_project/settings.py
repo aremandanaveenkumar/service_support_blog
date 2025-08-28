5fa28dbbdb68be79d2ee28e0a0ad51413be2c268
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-08-28 16:17:54 +0200

    Fix error with summernotewidget iframe:false, width:100% in settings.py

--- service_support_project/settings.py
+++ service_support_project/settings.py
@@ -163,3 +163,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SUMMERNOTE_CONFIG = {
+    'iframe': False,  # Use inline mode
+    'summernote': {
+        'width': '100%',
+    },
+}
