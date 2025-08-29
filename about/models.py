c7c42278537015f50107f3149278b0074566e5ce
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-08-29 13:05:45 +0200

    Add all model entity relationship diagrams

--- about/models.py
+++ about/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=200)
@@ -10,7 +11,8 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class CollaborateRequest(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
